b65d47bcc4f057f514c1da4308f673c3b5120bf1
Quinnlan Hipp <99678158+quinnhipp@users.noreply.github.com>
2023-06-19 18:01:44 -0400

    Began Address form and component

---
+++ frontend/components/Address.tsx
@@ -0,0 +1,11 @@
+import { Section } from "@/forms/Base";
+
+export default function Address() {
+  return (
+    <>
+      <Section>
+        <h2>Hello</h2>
+      </Section>
+    </>
+  );
+}
--- frontend/pages/addressbook.tsx
+++ frontend/pages/addressbook.tsx
@@ -1,3 +1,4 @@
+import Address from "@/components/Address";
 import Navbar from "@/components/Navbar";
 
 export default function AddressBook() {
@@ -5,6 +6,7 @@ export default function AddressBook() {
     <div>
       <Navbar active="Tasks" />
       <div>Address Book</div>
+      <Address />
     </div>
   );
 }
