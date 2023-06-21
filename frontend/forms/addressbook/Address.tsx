82b1f6abcc8f28ed63bf0d45fae8ca1d43fa4061
Quinnlan Hipp <99678158+quinnhipp@users.noreply.github.com>
2023-06-21 15:49:04 -0400

    Added stuff to Address Book page and form

--- frontend/forms/addressbook/Address.tsx
+++ frontend/forms/addressbook/Address.tsx
@@ -1,3 +1,37 @@
-export default function Address() {
-  return <></>;
+import { InputGrid } from "@/components/Inputs";
+import { basicInput, basicSelect, basicTextArea } from "@/utilities/form";
+import options from "tailwind-datepicker-react/types/Options";
+import { Section } from "../Base";
+import { useImmer } from "use-immer";
+
+export default function AddressForm() {
+  const address = useImmer({
+    name: "Quinn Hipp",
+    organization: "Pet Cemetery",
+    email: "qhipp@noemail.com",
+    phone_number: "4195777420",
+    fax_number: "",
+    website: "qhipp.dev",
+    notes: "The coolest guy",
+    address: "123 Sesame St",
+  });
+
+  return (
+    <>
+      <InputGrid>
+        {basicInput({ id: "name", label: "Contact Name" }, address)}
+        {basicInput({ id: "organization", label: "Organization" }, address)}
+      </InputGrid>
+      <InputGrid>
+        {basicInput({ id: "email", label: "Email" }, address)}
+        {basicInput({ id: "phone_number", label: "Phone Number" }, address)}
+      </InputGrid>
+      <InputGrid>
+        {basicInput({ id: "fax_number", label: "Fax Number" }, address)}
+        {basicInput({ id: "website", label: "Website" }, address)}
+      </InputGrid>
+      {basicInput({ id: "address", label: "Address" }, address)}
+      {basicTextArea({ id: "notes", label: "Notes" }, address)}
+    </>
+  );
 }
--- frontend/pages/addressbook.tsx
+++ frontend/pages/addressbook.tsx
@@ -1,12 +1,109 @@
-import Address from "@/components/Address";
+import { EditButton, TrashButton } from "@/components/Icons";
+import Modal, { StandardModal } from "@/components/Modal";
 import Navbar from "@/components/Navbar";
+import Base, { Section } from "@/forms/Base";
+import ListView, { ListCard } from "@/forms/ListView";
+import AddressForm from "@/forms/addressbook/Address";
+import { Address } from "@/models/Address";
+import { useState } from "react";
+
+export function AddressCard({
+  address,
+  onClick,
+}: {
+  address: Address;
+  onClick: any;
+}) {
+  const header = (
+    <div className="flex">
+      <a
+        className="hover:text-sky-300 underline text-lg font-medium"
+        href={`${address.organization}`}
+      >
+        {address.organization}
+      </a>
+      <EditButton addClass="ml-auto" title="Edit Address" onClick={onClick} />
+      <TrashButton title="Delete Address" />
+    </div>
+  );
+  const leftSide = [
+    <>Contact Name: {address.name}</>,
+    <>Phone: {address.phone_number}</>,
+    <>
+      Email:
+      <a className="underline ml-1" href="#">
+        {address.email}
+      </a>
+    </>,
+    <>
+      Website:
+      <a className="underline ml-1" href={address.website}>
+        {address.website}
+      </a>
+    </>,
+  ];
+  const rightSide = [
+    <>Cemetery: {address.organization}</>,
+    <>
+      Address:
+      <a className="underline ml-1" href="#">
+        {address.address}
+      </a>
+    </>,
+  ];
+
+  return (
+    <ListCard
+      leftFields={leftSide}
+      rightFields={rightSide}
+      footer=""
+      header={header}
+    ></ListCard>
+  );
+}
 
 export default function AddressBook() {
+  const [showModal, setShowModal] = useState(false);
+
+  const onSubmit = () => {};
+
+  const onCancel = () => {
+    setShowModal(false);
+  };
+
+  const addresses: Array<Address> = [
+    {
+      name: "Quinn Hipp",
+      organization: "Pet Cemetery",
+      email: "qhipp@noemail.com",
+      phone_number: "4195777420",
+      fax_number: "",
+      website: "https://qhipp.dev",
+      notes: "The coolest guy",
+      address: "123 Sesame St",
+    },
+  ];
+  const addressElements = addresses.map((x) => (
+    <AddressCard key={x as any} address={x} onClick={setShowModal} />
+  ));
+
   return (
     <div>
-      <Navbar active="Tasks" />
-      <div>Address Book</div>
-      <Address />
+      <Navbar active="Address Book" />
+      <StandardModal
+        showModal={showModal}
+        title=""
+        onSubmit={onSubmit}
+        onCancel={onCancel}
+      >
+        <AddressForm />
+      </StandardModal>
+      <ListView searchPlaceholder="Search orders..." filter="Hello">
+        {addressElements}
+      </ListView>
+      {/* <Base sectionHeader="Address Book" className="mt-10">
+        <AddressCard address={addresses[0]} onClick={setShowModal} />
+      </Base> */}
     </div>
   );
 }
