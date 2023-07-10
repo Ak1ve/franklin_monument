19cfd77b9a68bb4a81688b2d1ee39d23cf8a7c22
Quinnlan Hipp <99678158+quinnhipp@users.noreply.github.com>
2023-07-10 17:09:12 -0400

    Fixed the Draggable Task List

--- frontend/forms/tasks/Tasks.tsx
+++ frontend/forms/tasks/Tasks.tsx
@@ -109,14 +109,16 @@ export default function TaskList(this: any) {
           New Item
         </StandardButton>
       </div>
+      {!showModal && (
+        <div style={{ width: 1000, margin: "0 auto" }}>
+          <DraggableList width={1000} height={100} rowSize={1}>
+            {tasks.map((x) => (
+              <TaskCard key={x as any} task={x} onClick={setShowModal} />
+            ))}
+          </DraggableList>
+        </div>
+      )}
 
-      <div style={{ width: 1000, margin: "0 auto" }}>
-        <DraggableList width={1000} height={100} rowSize={1}>
-          {tasks.map((x) => (
-            <TaskCard key={x as any} task={x} onClick={setShowModal} />
-          ))}
-        </DraggableList>
-      </div>
       {/* {taskElements} */}
       {/* <div>
         <div>{width}</div>
