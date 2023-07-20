0a7aa9cc7bc1c401c7dd9e47cf4d34e1739e8fdf
Quinnlan Hipp <99678158+quinnhipp@users.noreply.github.com>
2023-07-20 14:51:44 -0400

    Added bases and sections to User tabs

--- frontend/forms/users/Admin.tsx
+++ frontend/forms/users/Admin.tsx
@@ -1,3 +1,11 @@
+import Base, { Section } from "../Base";
+
 export default function Admin() {
-  return <div>Admin</div>;
+  return (
+    <Base sectionHeader="Admin">
+      <Section className="mt-10">
+        <h2 className="section-header mb-5">Big Headstone</h2>
+      </Section>
+    </Base>
+  );
 }
--- frontend/forms/users/Information.tsx
+++ frontend/forms/users/Information.tsx
@@ -1,3 +1,11 @@
+import Base, { Section } from "../Base";
+
 export default function Information() {
-  return <div>Information</div>;
+  return (
+    <Base sectionHeader="Information">
+      <Section className="mt-10">
+        <h2 className="section-header mb-5">Big Headstone</h2>
+      </Section>
+    </Base>
+  );
 }
