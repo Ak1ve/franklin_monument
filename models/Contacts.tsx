0db671784d9345bcf4cc26953bd142c4671bd5cf
Ak1ve <rockwell.austin04@gmail.com>
2023-08-22 12:06:00 -0400

    Removed Models folder

--- models/Contacts.tsx
+++
@@ -1,9 +0,0 @@
-export interface Contact {
-    name: Optional<string>,
-    organization: Optional<string>,
-    email: Optional<string>,
-    phoneNumber: Optional<string>,
-    faxNumber: Optional<string>,
-    website: Optional<string>,
-    notes: Optional<string>
-}
