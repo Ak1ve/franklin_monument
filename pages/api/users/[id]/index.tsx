ef8cc2b57eba0897447989e7f3e9849cb7331b14
Ak1ve <rockwell.austin04@gmail.com>
2023-08-23 14:54:27 -0400

    a

---
+++ pages/api/users/[id]/index.tsx
@@ -0,0 +1,25 @@
+import { UserSelectOptions } from "@/data/models/user";
+import { Data } from "@/data/schema";
+import { endpoint, userParams, reqPerm, divyQueryId} from "@/utilities/endpoint";
+import { PrismaClient } from "@prisma/client";
+
+
+
+const prisma = new PrismaClient();
+
+
+export default endpoint({
+    getParams: userParams<UserSelectOptions>(true),
+    get: divyQueryId(["select"],
+    // select
+    async ({ req, res }) => {
+        const users = await prisma.user.findMany({
+            select: {
+                id: true,
+                name: true
+            },
+        });
+        const a = users!.map(x => ({label: x.name || "", value: x.id.toString()}));
+        res.status(200).json(a);
+    })
+})
--- utilities/listcard.tsx
+++ utilities/listcard.tsx
@@ -16,11 +16,11 @@ export interface ListCardProps<D> {
     refresh: () => void
     isError: boolean
     errorMessage: any
+    deleteId: (id: number) => any
 }
 
 
 export function listCard<D>(schema: ModelSchema<D>, component: (props: ListCardProps<D>) => JSX.Element, fetchOnDock: boolean = true, navBase: boolean = true): () => JSX.Element {
-
     return () => {
         const sessionStatus = useSession({ required: true }).status;
         const [data, setData] = useState(null as D);
@@ -29,6 +29,13 @@ export function listCard<D>(schema: ModelSchema<D>, component: (props: ListCardP
         const showInfoHook = useState(false);
         const refreshHook = useState(false);
         const [fetched, setFetched] = useState(false);
+        const [errorMessage, setErrorMessage] = useState(null as null | string);
+        const [showMessage, setShowMessage] = useState(false);
+
+        const refresh = () => {
+            refreshHook[1]((x) => !x);
+        }
+
         useEffect(() => {
             if (!fetchOnDock && !fetched) {
                 setFetched(true);
@@ -50,21 +57,37 @@ export function listCard<D>(schema: ModelSchema<D>, component: (props: ListCardP
                 }
             }))
         }, [refreshHook[0]]);
+
+        const deleteId = (id: number) => {
+            schema.route.delete!(getRouteParams({
+                router, schema, path: router.asPath + "/" + id.toString(),
+                onSuccess(success) {
+                    refresh();
+                },
+                onError(err) {
+                    if (err.error.type === "Network") {
+                        (err.error.error.info as any).then((x: EndpointError) => {
+                            setErrorMessage(`Error: Could not delete. ${JSON.stringify(x)}`);
+                            setShowMessage(true);
+                        });
+                    }
+                }
+            }));
+        }
+
+
         setTimeout(() => showInfoHook[1](true), 5000);
 
     const isLoading = sessionStatus === "loading" || data === null;
 
         const isPermError = endpointError?.type === "Permission";
-        
+
         const query = (callback: (query: Object) => Object) => {
-            return router.push({query: callback(router.query as any) as any});
+            return router.push({ query: callback(router.query as any) as any });
         }
 
-        const refresh = () => {
-            refreshHook[1]((x) => !x);
-        }
-
-        return component(
+        return (<>
+        {component(
             {
                 data,
                 isLoading,
@@ -79,7 +102,7 @@ export function listCard<D>(schema: ModelSchema<D>, component: (props: ListCardP
                 endpointError: endpointError,
                 query,
                 refresh,
-                isError: endpointError === null,
+                isError: endpointError !== null,
                 errorMessage: (<>
                     {navBase && <Navbar active="******" />}
                     <StandardError ok="Back to Dashboard" onOk={() => { router.push("/dashboard") }}
@@ -96,8 +119,75 @@ export function listCard<D>(schema: ModelSchema<D>, component: (props: ListCardP
                                 </>)
                         }
                     </StandardError>
-                </>)
+                </>),
+                deleteId
             }
-        );
+        )}
+            <Notification hook={[showMessage, setShowMessage]} type="danger">
+                {errorMessage}
+            </Notification>
+        </>)
     };
+
+    return component({
+      data,
+      isLoading,
+      loadingMessage: (
+        <>
+          {navBase && <Navbar active="******" />}
+          <StandardInfo
+            info="Loading page..."
+            continue="Back To Dashboard"
+            onContinue={() => router.push("/dashboard")}
+            hook={showInfoHook}
+          >
+            This page is currently loading. If this page loads for too long,
+            ensure the system is working properly. It is possible the data is
+            taking a while to load, or there is an error with how the data is
+            setup.
+            <br />
+            If you can't currently access this page, you can navigate back to
+            the dashboard.
+          </StandardInfo>
+        </>
+      ),
+      endpointError: endpointError,
+      query,
+      refresh,
+      isError: endpointError !== null,
+      errorMessage: (
+        <>
+          {navBase && <Navbar active="******" />}
+          <StandardError
+            ok="Back to Dashboard"
+            onOk={() => {
+              router.push("/dashboard");
+            }}
+            error={isPermError ? "Not Allowed" : endpointError?.type}
+            hook={[true, () => {}]}
+          >
+            {isPermError ? (
+              <>
+                You are not allowed to view this page. If this is a mistake,
+                contact an administrator. If you were recently granted
+                permission to view this page, please wait 5-10 minutes before
+                attempting to load again.
+                <br />
+                <br />
+                Additional information: {endpointError.error}
+              </>
+            ) : (
+              <>
+                An unknown error has occured while loading this page. Please
+                ensure that your account has been set up correctly.
+                <br />
+                <br />
+                Additional informaiton: ${endpointError?.error}
+              </>
+            )}
+          </StandardError>
+        </>
+      ),
+    });
+  };
 }
